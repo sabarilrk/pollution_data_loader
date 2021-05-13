fe1a27c44d5448596cd0ae562ff85ca27f208292
sabarilrk <sabarilrk@gmail.com>
2021-05-13 17:55:19 -0500

    added pydantic basemodel

--- Model/pollution_model.py
+++ Model/pollution_model.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import date
 
 class PollutionModel(BaseModel):
     id: Optional[int]
@@ -8,18 +8,25 @@ class PollutionModel(BaseModel):
     state: Optional[str]
     city: Optional[str]
     station: Optional[str]
-    last_update:Optional[datetime]
-    pollution_id: Optional[str]
-    pollution_min:Optional[int]
-    pollution_max: Optional[int]
-    pollution_avg: Optional[int]
-    pollution_unit: Optional[str]
+    last_update:Optional[str]
+    pollutant_id: Optional[str]
+    pollutant_min:Optional[str]
+    pollutant_max: Optional[str]
+    pollutant_avg: Optional[str]
+    pollutant_unit: Optional[str]
 
     class config:
         allow_population_by_field_name = True
         fields= {'id':{'alias':'id'},
-                'country':{'alias':'country'},
-                'state':{'alias':'state'},
-                'city':{'alias':'city'}
-         }
+                 'country':{'alias':'country'},
+                 'state':{'alias':'state'},
+                 'city':{'alias':'city'},
+                 'station':{'alias':'station'},
+                 'last_uppdate':{'alias':'last_update'},
+                 'pollutant_id':{'alias':'pollution_id'},
+                 'pollutant_min': {'alias': 'pollution_min'},
+                 'pollutant_max': {'alias': 'pollution_max'},
+                 'pollutant_avg': {'alias': 'pollution_avg'},
+                 'pollutant_unit': {'alias': 'pollution_unit'}
+        }
 
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import requests
 import json
+from Model.pollution_model import PollutionModel
 
 def get_pollution_date(url):
+    res_list=[]
     _url=url
     data = requests.get(url)
     data_dict = data.json()
 
     print(data_dict['records'])
+    for i in data_dict['records']:
+        trans_rec = transform(i)
+        res_list.append(list(trans_rec.dict().values()))
+    print(res_list)
+def transform(row:dict)->PollutionModel:
+    obj = PollutionModel(**row)
+    return obj
 
 if __name__ == '__main__':
     api_key = "579b464db66ec23bdd000001733643dae604467b4345307681dd876c"
-    url = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key={api_key}&format=json".format(api_key=api_key)
+    url = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key={api_key}&format=json&offset=0&limit=2000".format(api_key=api_key)
 
     get_pollution_date(url)
